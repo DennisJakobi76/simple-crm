b71b1560948be282badb5a17b364ddc6373da436
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-08-06 17:22:33 +0200

    Adds responsive sidenav and updates layout styling
    Introduces a responsive side navigation drawer to improve the user interface and layout. Enhances usability with a toggleable menu icon and updates global styles for better visual consistency. Also updates module imports to include required sidenav functionality.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [RouterOutlet, MatToolbarModule, MatSidenavModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
