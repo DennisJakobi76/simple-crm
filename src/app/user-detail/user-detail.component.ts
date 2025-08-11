b27c1884b7d3f40182bfce41c34d1b36a2fa318c
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-08-11 16:58:03 +0200

    Bind user details dynamically and fetch data from Firestore
    Replaces static user details in the template with dynamic bindings to display user data. Integrates Firestore to fetch user information based on route parameters. Implements a `getUser` method to retrieve and map Firestore data to the user model.

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { ActivatedRoute } from '@angular/router';
+import { User } from '../../models/user.class';
+import { Firestore, doc, getDoc } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-user-detail',
@@ -9,11 +11,20 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './user-detail.component.scss',
 })
 export class UserDetailComponent {
-  userId: string | undefined;
+  userId = '';
+  user: User = new User();
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private firestore: Firestore) {
     this.route.paramMap.subscribe((params) => {
       this.userId = params.get('id') || '';
+      this.getUser();
     });
+  }
+
+  async getUser() {
+    if (!this.userId) return;
+    const userDocRef = doc(this.firestore, `users/${this.userId}`);
+    const userSnap = await getDoc(userDocRef);
+    this.user = userSnap.exists() ? (userSnap.data() as User) : new User();
   }
 }
